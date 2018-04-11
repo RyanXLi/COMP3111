bcb45c4c7128e9a15227879fa0e38131f3059814
[Zhao] <[xzhaoar@gmail.com]>
2018-04-11 13:54:30 +0800

    finish loading the data and a naive fill

--- src/core/comp3111/LoadCSV.java
+++ src/core/comp3111/LoadCSV.java
@@ -1,7 +1,10 @@
 package core.comp3111;
 
+import java.io.FileNotFoundException;
 import java.io.BufferedReader; 
-import java.io.FileReader; 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /*
  * This is a function to load .csv to create a datatable
@@ -10,21 +13,25 @@ import java.io.FileReader;
 
 public class LoadCSV {
 	
-	public DataTable loadCSV(String fileName, String handleType) {
+	private static final int ColNum = 0;
+
+	public DataTable loadCSV(String fileName, String handleType) throws IOException {
 		
 		DataTable result = new DataTable();
+		int RowNum = 0;
+		int ColNum = 0;
+		
+		
 		
 		// Find the size of the table
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(fileName));
 			String line = null;
-			int RowNum = 0;
-			int ColNum = 0;
 			
 			while ((line = reader.readLine()) != null) {
-				String[] items = line.split(",");
-				if (items.length > RowNum) {
-					RowNum = items.length;
+				String[] item = line.split(",");
+				if (item.length > RowNum) {
+					RowNum = item.length;
 				}
 				ColNum = ColNum + 1;
 			}
@@ -32,7 +39,37 @@ public class LoadCSV {
 				System.out.println("Given fileName cannot be processed");
 		}
 		
+		
+		// Load the data
+		ArrayList<ArrayList<String>> csvList = new ArrayList<ArrayList<String>>();
+		
+		BufferedReader reader2 = new BufferedReader(new FileReader(fileName));
+		String line2 = null;
+		
+		while ((line2 = reader2.readLine()) != null) {
+				String[] items = line2.split(",");
+				ArrayList<String> temp = null;
+				for (int i = 0; i < items.length; i++) {
+					temp.add(items[i]);
+				}
+				csvList.add(temp);
+		}
+		
+		
 		// Decide the filling staff
+		// There are two situations when we need fill: Row < RowNum and ,, for Num
+		String stringfill = "";
+		int numfill = 0;
+		
+		// first fill everything with ""
+		for(int i = 0; i < csvList.size(); i++) {
+			while (csvList.get(i).size() < ColNum) {
+				csvList.get(i).add(stringfill);
+			}
+		}
+		
+		// Transpose the Arraylist
+		
 			
 			
 			
