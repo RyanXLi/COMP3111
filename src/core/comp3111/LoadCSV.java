9f1aae352ac6be074a1602e77dc3c1e29f51e860
[Zhao] <[xzhaoar@gmail.com]>
2018-04-11 12:56:48 +0800

    Initialize the loadcsv function

---
+++ src/core/comp3111/LoadCSV.java
@@ -0,0 +1,11 @@
+package core.comp3111;
+
+/*
+ * This is a function to load .csv to create a datatable
+ * The inputs are the filename and miss data handling type
+ */
+
+public class LoadCSV {
+	
+	DataTable loadC
+}
