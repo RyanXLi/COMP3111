2ce3322f2fbf621b2e045e1c3bc30e25df86c04f
[Zhao] <[xzhaoar@gmail.com]>
2018-04-18 19:46:53 +0800

    finished load .comp3111

--- src/core/comp3111/EnvirHandler.java
+++ src/core/comp3111/EnvirHandler.java
@@ -5,7 +5,9 @@ import java.io.*;
 
 /*
  * This is a function to handle save and load of .comp3111
- * The inputs are current DataCollection function(S or L) and the filename
+ * The inputs are current DataCollection function(S or L) 
+ * and the filename(with .comp3111 as the end)
+ * I wonder if I need to output something
  */
 
 public class EnvirHandler {
@@ -20,6 +22,7 @@ public class EnvirHandler {
 		if (function == "S") {
 			File file = new File("3111save"+ File.separator + filename);
 			FileOutputStream fos = null;
+			
 			try {
 				fos = new FileOutputStream(file);
 				ObjectOutputStream oos = null;
@@ -46,6 +49,37 @@ public class EnvirHandler {
 				}
 			}
 		} else if (function == "L") {
+			File file2 = new File("3111save"+ File.separator + filename);
+			FileInputStream fis = null;
+			try {
+				fis = new FileInputStream(file2);
+				ObjectInputStream ois = null;
+				try {
+					ois = new ObjectInputStream(fis);
+					try {
+						output = (DataCollection)ois.readObject();
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						ois.close();
+					} catch (IOException e) {
+						System.out.println("fail to close ois" + e.getMessage());
+					}
+				}
+				
+			} catch (FileNotFoundException e) {
+				System.out.println("file not found: " + e.getMessage());
+			} finally {
+				try {
+					fis.close();
+				} catch (IOException e) {
+					System.out.println("fail to close fis"+ e.getMessage());
+				}
+			}
 			
 		}
 		
