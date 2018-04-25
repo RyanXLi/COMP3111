c24029d5af3242e0632d5b1fb6c176c85aef2d7a
[Zhao] <[xzhaoar@gmail.com]>
2018-04-25 16:09:40 +0800

    Implemented saving to .csv, added getMap to DataTable

---
+++ src/core/comp3111/SaveCSV.java
@@ -0,0 +1,39 @@
+package core.comp3111;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.*;
+import java.util.*;
+
+/*
+ * This is a function to save dataTable to a csv file
+ * The inputs are the filename and the dataTable you want to save
+ */
+
+public class SaveCSV {
+
+	public static void loadCSV(String fileName, DataTable source) throws IOException, DataTableException {
+		
+		Map<String, DataColumn> result = new HashMap<>(source.getMap());
+		
+		File csv = new File(fileName); 
+        BufferedWriter bw = new BufferedWriter(new FileWriter(csv)); 
+		
+		for (int i = 1; i <= source.getNumCol(); i++) {
+			for (Map.Entry<String, DataColumn> entry: result.entrySet()) {
+					Object[] temp = ((DataColumn) entry).getData();// Current Data Column
+					bw.write((String)temp[i-1]);
+					if (i != source.getNumCol()) {
+						bw.write(",");
+					}
+			}
+			bw.newLine();
+		}
+		bw.close();
+		
+	}	
+}
